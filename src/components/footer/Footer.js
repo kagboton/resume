e88cafed800a0afc23a4fd615ac0ea6b743cada9
Kilian Agboton <kilian.agboton@gmail.com>
2021-01-24 18:57:19 -0500

    completed footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,22 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import './Footer.css'
+import data from '../../utils/resumeData'
+
 
 const Footer = () => {
     return (
-        <div>
-            Footer component
+        <div className='footer'>
+            <div className='footer-left'>
+                <Typography className='footer-name'>{data.name}</Typography>
+            </div>
+            <div className='footer-right'>
+                <Typography className='footer-copyright'>
+                    Designed and Developed by <a href='#'>Kilian Agboton</a>
+                    <br />
+                    Clone idea from <a href='https://themeforest.net/user/tavonline' target='_blank'>tavonline</a>.
+                </Typography>
+            </div>
         </div>
     )
 }
